55298ee110657590a785c74ecff84817111c417f
snvibes9 <snvibes9@gmail.com>
2025-07-01 11:11:23 +0530

    Update sns.tf
    Update sns.tf

--- modules/sns.tf
+++ modules/sns.tf
@@ -1,15 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.43" # Use a stable recent version that passes security checks
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region                  = "us-east-1"
+}
+
 locals {
   common_tags = {
     Project     = "AFT-Pipeline-Notifications"
     Environment = "prod"
-    Owner       = "cloud-platform"
-    CostCenter  = "CC1234"
+    CostCenter  = ""
   }
 }
 
 module "sns" {
-  source      = "./modules/sns_notify"
+  source      = "../modules/sns_notify"
   topic_name  = "aft-pipeline-topic"
   tags        = local.common_tags
-  subscribers = ["secops@example.com", "devops@example.com"]
+  subscribers = ["snvibes9@gmail.com"]
+}
+
+module "event_rule" {
+  source        = "../modules/event_rule_pipeline_notify"
+  rule_name     = "aft-pipeline-notify-rule"
+  sns_topic_arn = "arn:aws:sns:us-east-1:682475224788:aft-pipeline-topic"
+  tags = local.common_tags
 }
