30740a28a5e7bd68d32d2446ab6a9a583f9a7169
snvibes9 <snvibes9@gmail.com>
2025-07-16 23:32:56 +0530

    Update event.tf

--- aft/event.tf
+++ aft/event.tf
@@ -1,1 +1,16 @@
-
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+        "Effect": "Allow"
+        "Principal": "events.amazonaws.com"
+      },
+      "Action": "sts.AssumeRole"
+      "Condition": {
+          "StringEquals": {
+            "aws:SourceArn": "arn:aws:events:us-east-1:xxxxx:rule/CodePipelineComplete",
+            "aws:SourceAccount": "xxxxxx"
+          }
+      }
+  ]
+}
